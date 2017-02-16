28cd4f9faa9a8663940c6b1cc90bf9a1f92d80af
miscbits <willow6786@gmail.com>
2017-02-16 01:20:48 -0500

    added payment form and tested

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::post('/payment', ['uses' => 'PaymentController@payment', 'middleware' => 'auth']);
